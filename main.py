6defd522b3afeafeef9377ac9b25d58fba9c6301
Dar1aBekreneva <bekreneva225@gmail.com>
2023-03-14 16:07:37 +0300

    lab3.1

---
+++ main.py
@@ -0,0 +1,12 @@
+from random import randint
+k = 0
+while k != 3:
+    a = randint(1, 100)
+    b = randint(1, 100)
+    print(a, '+', b, '= ')
+    c = int(input())
+    if c == a + b:
+        print('good, mistakes: ', k)
+    else:
+        k += 1
+        print('bad, mistakes: ', k)

ab2509b88a2803650a5e38c845ca674e7dbf3786
Dar1aBekreneva <bekreneva225@gmail.com>
2023-03-14 14:04:57 +0300

    lab3

---
+++ main.py
@@ -0,0 +1,29 @@
+def q0():
+    m = int(input())
+    n = []
+    for i in range(m):
+        n.append(input())
+    w = ' '.join(n)
+    print(w)
+
+def q1():
+    n = []
+    m = str(input())
+    while m != 'stop':
+        m = str(input())
+        n.append(m)
+    w = ' '.join(n)
+    print(w)
+
+def q2():
+    n = str(input())
+    l = "ф"
+    x = l in n
+    if x == True:
+        print("редкое слово")
+    else:
+        print("обычное слово")
+
+q0()
+q1()
+q2()
